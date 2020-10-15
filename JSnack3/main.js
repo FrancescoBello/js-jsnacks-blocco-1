8dd880f3326dd9147d5cdb5f891f0deb7884eace
Francesco <fraancesco.bello@gmail.com>
2020-10-15 17:35:39 +0200

    JSnack3

---
+++ JSnack3/main.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+var numero1 = prompt('Inserisci un numero')
+var numero2 = prompt('Inserisci un numero')
+var numero3 = prompt('Inserisci un numero')
+var numero4 = prompt('Inserisci un numero')
+var numero5 = prompt('Inserisci un numero')
+
+var tot = [numero1, numero2, numero3, numero4, numero5]
+
+for (var i = 0; i < tot.length; i++) {
+  tot[i]
+  console.log(tot);
+}
